5324da7e91e7e2d07cb0926840bcce064624130c
cburstion87 <30274364+cburstion87@users.noreply.github.com>
2017-07-19 20:14:56 -0400

    Add files via upload

---
+++ LNT.py
@@ -0,0 +1,48 @@
+'''print ("Welcome to Late Night Trill")
+'''
+'''
+test1 = int(input ("What is your first score?"))
+test2 = int(input ("Whats your second score?"))
+test3 = int(input ("What is your third score?"))
+average = (test1+test2+test3)/3
+print ("Your average is", average)
+'''
+'''
+print (3-4)
+print (3+4)
+print (3*4)
+print (3/4)
+print (3%2)
+print (3**4)
+print (3//4)
+'''
+'''
+a=0
+a+=2
+print (a)
+'''
+'''print ("My name is ",end="")
+print ("Ecrutony")'''
+'''def main():
+    average (5,7)
+def average (a,b):
+    print ((a+b)/2)
+main()'''
+'''a=10
+def somefunction():
+    print (a+5)
+somefunction()'''
+'''a = 20
+if a >= 22:
+    print("if")
+elif a >= 21:
+    print ("elif")
+else:
+    print ("else")'''
+test = int(input("What is the weather outside?"))
+if test >=60 and test <=90:
+    print ("Wharm")
+elif test >=90:
+    print ("HAWT")
+if test <=60:
+    print("Cald")
